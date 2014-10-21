b11e64b8f31c124104a1104a30f1c152f6c88785
Jelte Fennema <github-tech@jeltef.nl>
2014-10-21 19:17:14 +0200

    Make a general has_set function to make has_pair general

--- src/combos.rs
+++ src/combos.rs
@@ -2,12 +2,22 @@ use cards::Card;
 
 // All these functions need reverse sorted vectors of Cards
 pub fn has_pair(card_vec: &Vec<&Card>) -> bool {
+    has_set(2, card_vec)
+}
+
+fn has_set(set_size: u8, card_vec: &Vec<&Card>) -> bool {
     let mut prev: &Card = card_vec[0];
+    let mut count: u8 = 1;
     for card in card_vec.iter().skip(1) {
         if *card == prev {
-            return true;
+            count += 1;
+            if count == set_size {
+                return true
+            }
+            continue;
         }
-        prev = *card
+        prev = *card;
+        count = 1;
     }
-    return false;
+    false
 }
