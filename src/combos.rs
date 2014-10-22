edd664fb912495413fc1a571210ebd3ebba1659b
Jelte Fennema <github-tech@jeltef.nl>
2014-10-22 18:19:11 +0200

    Add three/four of a kind and full house checks

--- src/combos.rs
+++ src/combos.rs
@@ -5,6 +5,14 @@ pub fn has_pair<'r>(card_vec: &Vec<&'r Card>) -> Option<&'r Card> {
     has_set(2, card_vec)
 }
 
+pub fn has_three_of_a_kind<'r>(card_vec: &Vec<&'r Card>) -> Option<&'r Card> {
+    has_set(3, card_vec)
+}
+
+pub fn has_four_of_a_kind<'r>(card_vec: &Vec<&'r Card>) -> Option<&'r Card> {
+    has_set(4, card_vec)
+}
+
 fn has_set<'r>(set_size: u8, card_vec: &Vec<&'r Card>) -> Option<&'r Card> {
     let mut prev = match card_vec.last() {
         Some(x) => x,
@@ -26,6 +34,16 @@ fn has_set<'r>(set_size: u8, card_vec: &Vec<&'r Card>) -> Option<&'r Card> {
     None
 }
 
+pub fn has_two_pair<'r>(
+        card_vec: &Vec<&'r Card>) -> Option<(&'r Card, &'r Card)> {
+    has_sets(2, 2, card_vec)
+}
+
+pub fn has_full_house<'r>(
+        card_vec: &Vec<&'r Card>) -> Option<(&'r Card, &'r Card)> {
+    has_sets(3, 2, card_vec)
+}
+
 fn has_sets<'r>(set_size1: u8, set_size2: u8,
                 card_vec: &Vec<&'r Card>) -> Option<(&'r Card, &'r Card)> {
     let largest = match has_set(set_size1, card_vec) {
@@ -42,9 +60,4 @@ fn has_sets<'r>(set_size1: u8, set_size2: u8,
         None => return None
     };
     Some((largest, smallest))
-}
-
-pub fn has_two_pair<'r>(
-        card_vec: &Vec<&'r Card>) -> Option<(&'r Card, &'r Card)> {
-    has_sets(2, 2, card_vec)
 }
