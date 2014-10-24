d6ed6e1485f7be5f28ba4826121ab760cca6f185
Jelte Fennema <github-tech@jeltef.nl>
2014-10-24 21:52:51 +0200

    More tests

--- tests/combos.rs
+++ tests/combos.rs
@@ -31,3 +31,49 @@ fn test_incorrect_two_pair() {
     let card = combos::has_two_pair(&card_vec);
     assert_eq!(card, None);
 }
+
+#[test]
+fn test_correct_three_of_a_kind() {
+    let mut card_vec = Card::new_card_vec([(6, 0), (8, 2), (6, 3), (6, 1), (8, 1)]);
+    card_vec.sort();
+    let card = combos::has_two_pair(&card_vec);
+    assert_eq!(card, Some((Card::new(8, 0), Card::new(6, 0))));
+}
+#[test]
+fn test_incorrect_three_of_a_kind() {
+    let mut card_vec = Card::new_card_vec([(14, 2), (7, 3), (7, 1), (8, 1)]);
+    card_vec.sort();
+    let card = combos::has_two_pair(&card_vec);
+    assert_eq!(card, None);
+}
+
+#[test]
+fn test_correct_full_house() {
+    let mut card_vec = Card::new_card_vec([(6, 0), (8, 2), (6, 3), (6, 1), (8, 1)]);
+    card_vec.sort();
+    let card = combos::has_two_pair(&card_vec);
+    assert_eq!(card, Some((Card::new(8, 0), Card::new(6, 0))));
+}
+#[test]
+fn test_incorrect_full_house() {
+    let mut card_vec = Card::new_card_vec([(7, 2), (7, 3), (7, 1), (8, 1)]);
+    card_vec.sort();
+    let card = combos::has_two_pair(&card_vec);
+    assert_eq!(card, None);
+}
+
+#[test]
+fn test_correct_four_of_a_kind() {
+    let mut card_vec = Card::new_card_vec([(6, 0), (8, 2), (6, 3), (6, 1), (8, 1), (6, 2)]);
+    card_vec.sort();
+    let card = combos::has_two_pair(&card_vec);
+    assert_eq!(card, Some((Card::new(8, 0), Card::new(6, 0))));
+}
+#[test]
+fn test_incorrect_four_of_a_kind() {
+    let mut card_vec = Card::new_card_vec([(14, 2), (7, 3), (7, 1), (8, 1)]);
+    card_vec.sort();
+    let card = combos::has_two_pair(&card_vec);
+    assert_eq!(card, None);
+}
+
